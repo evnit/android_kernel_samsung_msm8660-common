9ce43877c2d6b1d503cea67a9e307bd67c43a4a3
Sultanxda <sultanxda@gmail.com>
2015-01-25 11:50:40 -0800

    input: cypress_touchkey: Fix NULL pointer in i2c_touchkey_write()
    Enhanced BLN already accounts for this, so this check is useless anyway.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -204,7 +204,6 @@ struct i2c_touchkey_driver {
 	struct early_suspend early_suspend;
         atomic_t keypad_enable;
 	struct mutex mutex;
-	bool is_bln_active;
 };
 struct i2c_touchkey_driver *touchkey_driver = NULL;
 struct work_struct touchkey_work;
@@ -302,11 +301,7 @@ static int i2c_touchkey_write(u8 * val, unsigned int len)
 	struct i2c_msg msg[1];
 	int retry = 2;
 
-	if ((touchkey_driver == NULL || touchkey_enable != 1)
-#if defined(CONFIG_ENHANCED_BLN)
-		&& (!touchkey_driver->is_bln_active)
-#endif
-	) {
+	if ((touchkey_driver == NULL || touchkey_enable != 1)) {
 		printk(KERN_DEBUG "[TKEY] touchkey is not enabled.W\n");
 		return -ENODEV;
 	}
@@ -1058,7 +1053,6 @@ static void cypress_touchkey_enable_led_vdd(void)
 	tkey_vdd_enable(1);
 	msleep(50);
 	tkey_led_vdd_enable(1);
-	touchkey_driver->is_bln_active = true;
 	mutex_unlock(&touchkey_driver->mutex);
 }
 
@@ -1067,7 +1061,6 @@ static void cypress_touchkey_disable_led_vdd(void)
 	mutex_lock(&touchkey_driver->mutex);
 	tkey_vdd_enable(0);
 	tkey_led_vdd_enable(0);
-	touchkey_driver->is_bln_active = false;
 	mutex_unlock(&touchkey_driver->mutex);
 }
 
